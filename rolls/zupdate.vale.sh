d38b4ebee6baef1318d54675805930304737161d
Cameron Blackwood <cameron.blackwood@gmail.com>
2021-03-10 02:58:35 +1100

    script to pull googlesheets and do all the work needed

--- rolls/zupdate.vale.sh
+++ rolls/zupdate.vale.sh
@@ -1,32 +1,23 @@
 #!/bin/sh
 
-OUT='/nas/1t-mirror/korg/sites/appengine/2-24.battalion.org.au/www/.build/'
+OUT='/nas/1t-mirror/korg/sites/appengine/2-24.battalion.org.au/www/.build.txt'
 
 
-echo "[BEGIN]" > $OUT/index.html
-date >> $OUT/index.html
+echo "[BEGIN]" > $OUT
 
-echo "Pulling google sheets...."  >> $OUT/index.html
-echo "" >> $OUT/index.html
-echo "" >> $OUT/index.html
 
 cd pull.spreadsheets
-python3 zpullsheets.py   >> $OUT/index.html 2>&1
+sh ./zrun.sh  >> $OUT 2>&1
 
-echo "" >> $OUT/index.html
-echo "----------------------" >> $OUT/index.html
-echo "" >> $OUT/index.html
-
-echo "Pulling google sheets...." >> $OUT/index.html
-echo "" >> $OUT/index.html
-echo "" >> $OUT/index.html
 
 cd ..
+
+
 cd vale.verify.install
-python3 zprocess.py    >> $OUT/index.html y 2>&1
+sh ./zrun.sh  >> $OUT 2>&1
 
 
-echo "[DONE]" >> $OUT/index.html
-date >> $OUT/index.html
+echo "[DONE]" >> $OUT
+date >> $OUT
 
 
