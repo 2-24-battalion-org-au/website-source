4cf7986477f6ffa24c545d044b5c8aa208e31036
Cameron Blackwood <cameron.blackwood@gmail.com>
2021-03-20 01:54:19 +1100

    moved .build.txt temp file to repo as nightly builder doesnt have output tree

--- rolls/zupdate.vale.sh
+++ rolls/zupdate.vale.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-OUT='/nas/1t-mirror/korg/sites/appengine/2-24.battalion.org.au/www/.build.txt'
+OUT="../webpages/.build.txt"
 
-
-echo "[BEGIN]" > $OUT
+echo "[BEGIN]" > "$OUT"
 
 
 cd pull.spreadsheets
-sh ./zrun.sh  >> $OUT 2>&1
+sh ./zrun.sh  >> "../$OUT" 2>&1
 
 
 cd ..
 
 
 cd vale.verify.install
-sh ./zrun.sh  >> $OUT 2>&1
+sh ./zrun.sh  >> "../$OUT" 2>&1
 
 
-echo "[DONE]" >> $OUT
-date >> $OUT
+cd ..
+
+
+echo "[DONE]" >> "$OUT"
+date >> "$OUT"
 
 
