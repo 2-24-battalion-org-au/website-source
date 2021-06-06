502e67f4763f1b8ce2e256167aa04ac144955592
Cameron Blackwood <cameron.blackwood@gmail.com>
2021-06-06 21:01:03 +1000

    pull members list csv too

--- rolls/pull.spreadsheets/zpullsheets.py
+++ rolls/pull.spreadsheets/zpullsheets.py
@@ -5,3 +5,6 @@ OUT='pulled.spreadsheets'
 gf=fetch_google_sheet.GSfile("Master Vale Roll")
 gf.sheet('Service Men').save2csv(OUT+'/vale.csv')
 gf.sheet('Widows').save2csv(OUT+'/vale_widows.csv')
+
+gf=fetch_google_sheet.GSfile("1gb1b9t2BEtLT_NIpXwWuzKlkwf0ET_ECOypadWlqJTc")
+gf.sheet('2021').save2csv(OUT+'/members.csv')
